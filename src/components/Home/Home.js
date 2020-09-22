15e4e5dc616ebc1ebbacb7c91ad06e91671e1d4c
unknown <filippemkt@gmail.com>
2020-09-22 10:53:17 -0300

    Image insertion on the home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Jumbotron'
+import Image from 'react-bootstrap/Image'
 
 import "./Home.css";
 
@@ -8,7 +9,6 @@ function Home() {
   return (
     <Jumbotron fluid>
       <Container>
-
         <h1>Editora Castor®</h1>
         <p>Somos um grupo de amantes de livros ambiciosos, e decidimos criar uma editora para publicarmos todos os livros que
         julgamos válidos para outros amantes de livros como nós. Só no primeiro mês da editora, já tinhamos acolhido 3 projetos de livros diferentes para publicação,
@@ -19,8 +19,8 @@ function Home() {
         <p>Desde a orientação editorial privilegia, temas e estilos que se destacam pela diferença, ousadia e impacto. Não foi à toa que o título de estréia da Castor®,
         Fazendo Diques - Magé 75016, de Jout Coutê Netô, figurou na lista de mais vendidos do Brasil por várias semanas. Publicado em agosto de 2020, o relato do escritor
       que chocou ao retratar a geração de "Castores" virou polêmica e recebeu destaque da mídia.</p>
-
-      </Container>]
+      <Image src="/img/Home1.png" alt="Mulher segundo uma pasta" fluid />
+      </Container>
     </Jumbotron>
   );
 }
