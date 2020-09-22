ac07e2c5404d93ad2320913bac46c212cb517b37
HitchHikerTP <42329281+HitchHikerTP@users.noreply.github.com>
2020-09-22 14:48:20 -0300

    form do categoria funcionando

--- src/components/Categoria/FormCategoria/FormCategoria.js
+++ src/components/Categoria/FormCategoria/FormCategoria.js
@@ -3,9 +3,29 @@ import axios from "axios";
 import { Container, FormControl, InputGroup } from "react-bootstrap";
 import styled, { css } from "styled-components";
 
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid #507bff;
+  color: #507bff;
+  float: right;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+
+  ${(props) =>
+    props.primary &&
+    css`
+      background: palevioletred;
+      color: white;
+    `};
+`;
+
 export default (props) => {
   const [nome, setNome] = useState("");
   const [descricao, setDescricao] = useState("");
+
+  
 
   async function novaCategoria() {
     axios
@@ -19,23 +39,6 @@ export default (props) => {
         console.log(res);
       });
   }
-
-  const Button = styled.button`
-    background: transparent;
-    border-radius: 3px;
-    border: 2px solid #507bff;
-    color: #507bff;
-    float: right;
-    margin: 0 1em;
-    padding: 0.25em 1em;
-
-    ${(props) =>
-      props.primary &&
-      css`
-        background: palevioletred;
-        color: white;
-      `};
-  `;
 
   return (
     <Container md="auto">
